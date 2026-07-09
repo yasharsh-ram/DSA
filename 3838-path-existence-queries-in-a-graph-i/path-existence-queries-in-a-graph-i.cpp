54e6f318214f1901a24da26e59e1183da78dd561
yasharsh-ram <sca25mca008@dsu.edu.in>
2026-07-09 22:05:33 +0530

    Time: 17 ms (59.73%) | Memory: 224.2 MB (66.03%) - LeetSync

--- 3838-path-existence-queries-in-a-graph-i/path-existence-queries-in-a-graph-i.cpp
+++ 3838-path-existence-queries-in-a-graph-i/path-existence-queries-in-a-graph-i.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
-    vector<bool> pathExistenceQueries(int n, vector<int>& nums, int maxDiff, vector<vector<int>>& queries) {
-        int N=nums.size();
-        vector<int>comp(N);
-        comp[0]=0;
-        for(int i=1;i<N;i++){
-            if(nums[i]-nums[i-1]<=maxDiff){
-                comp[i]=comp[i-1];
-            }else{
-                comp[i]=comp[i-1]+1;
+    vector<bool> pathExistenceQueries(int n, vector<int>& nums, int maxDiff,
+                                      vector<vector<int>>& queries) {
+        int N = nums.size();
+        vector<int> comp(N);
+        comp[0] = 0;
+        for (int i = 1; i < N; i++) {
+            if (nums[i] - nums[i - 1] <= maxDiff) {
+                comp[i] = comp[i - 1];
+            } else {
+                comp[i] = comp[i - 1] + 1;
             }
         }
-        vector<bool>ans;
-        for(auto &q:queries){
-            ans.push_back(comp[q[0]]==comp[q[1]]);
+        vector<bool> ans;
+        for (auto& q : queries) {
+            ans.push_back(comp[q[0]] == comp[q[1]]);
         }
         return ans;
-        
     }
 };
