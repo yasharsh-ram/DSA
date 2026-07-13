7b1ed9db0b3f14251dc7abbef0f3c4e902574e20
yasharsh-ram <sca25mca008@dsu.edu.in>
2026-07-13 18:00:04 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (76.91%) - LeetSync

--- 1212-sequential-digits/sequential-digits.cpp
+++ 1212-sequential-digits/sequential-digits.cpp
@@ -1,18 +1,33 @@
+// class Solution {
+// public:
+//     vector<int> sequentialDigits(int low, int high) {
+//         string s = "123456789";
+//         vector<int> ans;
+//         for (int len = 2; len < 10; ++len) {
+//             for (int start = 0; start <= s.length() - len; ++start) {
+//                 int digit = stoi(s.substr(start, len));
+//                 if (digit >= low && digit <= high) {
+//                     ans.push_back(digit);
+//                 }
+//             }
+//         }
+//         return ans;
+//     }
+// };
 class Solution {
 public:
     vector<int> sequentialDigits(int low, int high) {
-        string s="123456789";
-        vector<int>ans;
-        for(int len=2;len<10;++len){
-            for(int start=0;start<=s.length()-len;++start){
-                int digit=stoi(s.substr(start,len));
-                if(digit>=low&& digit<=high){
-                    ans.push_back(digit);
+        vector<int> ans;
+        for (int i = 1; i <= 9; ++i) {
+            int num = i;
+            for (int j = i + 1; j <= 9; ++j) {
+                num = num * 10 + j;
+                if (num >= low && num <= high) {
+                    ans.push_back(num);
                 }
             }
-
         }
+        sort(ans.begin(),ans.end());
         return ans;
-        
     }
 };
