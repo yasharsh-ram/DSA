82e4109379e6c75bdf644cd9b864fcfe8813831f
yasharsh-ram <sca25mca008@dsu.edu.in>
2026-08-02 10:00:19 +0530

    Time: 2 ms (39.30%) | Memory: 11.6 MB (36.14%) - LeetSync

--- 909-stone-game/stone-game.cpp
+++ 909-stone-game/stone-game.cpp
@@ -1,24 +1,24 @@
-// class Solution {
-// public:
-//     bool stoneGame(vector<int>& piles) {
-//         int n=piles.size();
-//         int dp[100][100]={};
-//         for(int i=0;i<n;i++){
-//             dp[i][i]=piles[i];        
-//         }
-//         for(int len=2;len<=n;len++){
-//             for(int i=0;i+len-1<n;i++){
-//                 int j=i+len-1;
-//                 dp[i][j]=max(piles[i]-dp[i+1][j],piles[i]-dp[i][j-1]);
-//             }
-//         }
-//         return dp[0][n-1]>=0;
-        
-//     }
-// };
 class Solution {
 public:
     bool stoneGame(vector<int>& piles) {
-        return true;
+        int n=piles.size();
+        static int dp[501][501]={};
+        for(int i=0;i<n;i++){
+            dp[i][i]=piles[i];        
+        }
+        for(int len=2;len<=n;len++){
+            for(int i=0;i+len-1<n;i++){
+                int j=i+len-1;
+                dp[i][j]=max(piles[i]-dp[i+1][j],piles[j]-dp[i][j-1]);
             }
+        }
+        return dp[0][n-1]>=0;
+        
+    }
 };
+// class Solution {
+// public:
+//     bool stoneGame(vector<int>& piles) {
+//         return true;
+//             }
+// };
