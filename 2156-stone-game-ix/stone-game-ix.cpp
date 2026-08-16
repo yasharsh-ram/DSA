bf0aef7da14942061cb53b1d4bdb47bd129d997a
yasharsh-ram <sca25mca008@dsu.edu.in>
2026-08-16 20:28:35 +0530

    Time: 6 ms (43.65%) | Memory: 131.2 MB (69.29%) - LeetSync

--- 2156-stone-game-ix/stone-game-ix.cpp
+++ 2156-stone-game-ix/stone-game-ix.cpp
@@ -1,20 +1,22 @@
 class Solution {
 public:
     bool stoneGameIX(vector<int>& stones) {
-        int zeroes=0;
-        int ones=0;
-        int twos=0;
-        for(int i=0;i<stones.size();i++){
-            int r=stones[i]%3;
-            if(r==0)zeroes++;
-            else if(r==1)ones++;
-            else twos++;
+        int zeroes = 0;
+        int ones = 0;
+        int twos = 0;
+        for (int i = 0; i < stones.size(); i++) {
+            int r = stones[i] % 3;
+            if (r == 0)
+                zeroes++;
+            else if (r == 1)
+                ones++;
+            else
+                twos++;
         }
-        if(zeroes%2==0){
-            return ones>0 && twos>0;
-        }else{ 
-            return abs(ones-twos)>2;
+        if (zeroes % 2 == 0) {
+            return ones > 0 && twos > 0;
+        } else {
+            return abs(ones - twos) > 2;
         }
-        
     }
 };
