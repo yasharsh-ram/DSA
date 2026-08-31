b2b99fcf861d1b6e1916ee64687cd7832ff42766
yasharsh-ram <sca25mca008@dsu.edu.in>
2026-08-31 20:30:27 +0530

    Time: 2 ms (70.81%) | Memory: 124.6 MB (81.63%) - LeetSync

--- 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
+++ 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
@@ -8,30 +8,61 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+// class Solution {
+// public:
+//     vector<int> nodesBetweenCriticalPoints(ListNode* head) {
+//         vector<int> arr;
+//         ListNode* temp = head;
+//         while (temp != NULL) {
+//             arr.push_back(temp->val);
+//             temp = temp->next;
+//         }
+//         vector<int> crit;
+//         int ans = 0;
+//         for (int i = 1; i < arr.size()-1; i++) {
+//             if ((arr[i - 1] < arr[i] && arr[i] > arr[i + 1]) ||
+//                 (arr[i - 1] > arr[i] && arr[i] < arr[i + 1])) {
+//                 crit.push_back(i);
+//             }
+//         }
+//         if (crit.size() < 2)
+//             return {-1, -1};
+//         int mindis = INT_MAX;
+//         for (int i = 1; i < crit.size(); i++) {
+//             mindis = min(mindis, crit[i] - crit[i - 1]);
+//         }
+//         int maxdis = crit.back() - crit.front();
+//         return {mindis, maxdis};
+//     }
+// };
+
 class Solution {
 public:
     vector<int> nodesBetweenCriticalPoints(ListNode* head) {
-        vector<int> arr;
-        ListNode* temp = head;
-        while (temp != NULL) {
-            arr.push_back(temp->val);
-            temp = temp->next;
-        }
-        vector<int> crit;
-        int ans = 0;
-        for (int i = 1; i < arr.size()-1; i++) {
-            if ((arr[i - 1] < arr[i] && arr[i] > arr[i + 1]) ||
-                (arr[i - 1] > arr[i] && arr[i] < arr[i + 1])) {
-                crit.push_back(i);
+        ListNode* prev=head;
+        ListNode* curr=head->next;
+        ListNode* next=curr->next;
+        int index=1;
+        int firstct=-1;
+        int prevct=-1;
+        int mindis=INT_MAX;
+        while(curr->next!=NULL){
+            if((curr->val<prev->val&&curr->val<next->val)||(curr->val>prev->val&&curr->val>next->val)){
+                if(firstct==-1){
+                    firstct=index;
+                }else{
+                    int dis=index-prevct;
+                    mindis=min(mindis,dis);
+                }
+                prevct=index;
             }
+            prev = curr;
+            curr = curr->next;
+            next = curr->next;
+            index++;
         }
-        if (crit.size() < 2)
-            return {-1, -1};
-        int mindis = INT_MAX;
-        for (int i = 1; i < crit.size(); i++) {
-            mindis = min(mindis, crit[i] - crit[i - 1]);
-        }
-        int maxdis = crit.back() - crit.front();
-        return {mindis, maxdis};
+        if(firstct==-1||firstct==prevct)return{-1,-1};
+        int maxdis=prevct-firstct;
+        return{mindis,maxdis};
     }
 };
