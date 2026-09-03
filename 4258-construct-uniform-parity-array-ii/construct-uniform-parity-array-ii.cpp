80429f4e572d6250f4372abec0a6fdebbba4af31
yasharsh-ram <sca25mca008@dsu.edu.in>
2026-09-03 20:40:41 +0530

    Time: 0 ms (100.00%) | Memory: 166 MB (31.94%) - LeetSync

--- 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.cpp
+++ 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.cpp
@@ -1,18 +1,37 @@
+// class Solution {
+// public:
+//     bool uniformArray(vector<int>& nums1) {
+//            int n=nums1.size();
+//            int smalod=INT_MAX;
+//            for(int i=0;i<n;i++){
+//             if(nums1[i]%2!=0){
+//                 smalod=min(smalod,nums1[i]);
+//             }
+//            }
+//            if(smalod==INT_MAX)return true;
+//            for(int i=0;i<n;i++){
+//             if(nums1[i]<smalod&&nums1[i]%2==0)
+//                 return false;
+//            }
+//            return true;
+//     }
+// };
+
 class Solution {
 public:
     bool uniformArray(vector<int>& nums1) {
-           int n=nums1.size();
+        int n=nums1.size();
            int smalod=INT_MAX;
+           int smalevn=INT_MAX;
            for(int i=0;i<n;i++){
             if(nums1[i]%2!=0){
                 smalod=min(smalod,nums1[i]);
-            }
+            }else{
+            smalevn=min(smalevn,nums1[i]);
            }
-           if(smalod==INT_MAX)return true;
-           for(int i=0;i<n;i++){
-            if(nums1[i]<smalod&&nums1[i]%2==0)
-                return false;
            }
-           return true;
+           if(smalod==INT_MAX||smalevn==INT_MAX)return true;
+           return smalod<smalevn;
     }
 };
+           
