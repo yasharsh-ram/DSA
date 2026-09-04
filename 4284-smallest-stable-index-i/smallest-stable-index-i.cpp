3c736992d0ef09e86ebaf53e564fe8f0fc2e221c
yasharsh-ram <sca25mca008@dsu.edu.in>
2026-09-04 10:50:47 +0530

    Time: 4 ms (13.74%) | Memory: 31.6 MB (5.99%) - LeetSync

--- 4284-smallest-stable-index-i/smallest-stable-index-i.cpp
+++ 4284-smallest-stable-index-i/smallest-stable-index-i.cpp
@@ -1,10 +1,31 @@
+// class Solution {
+// public:
+//     int firstStableIndex(vector<int>& nums, int k) {
+//         for(int i=0;i<nums.size();i++){
+//             int mx=*max_element(nums.begin(),nums.begin()+i+1);
+//             int mn=*min_element(nums.begin()+i,nums.end());
+//             if(mx-mn<=k)return i;
+//         }
+//         return -1;
+//     }
+// };
+
 class Solution {
 public:
     int firstStableIndex(vector<int>& nums, int k) {
-        for(int i=0;i<nums.size();i++){
-            int mx=*max_element(nums.begin(),nums.begin()+i+1);
-            int mn=*min_element(nums.begin()+i,nums.end());
-            if(mx-mn<=k)return i;
+        int n=nums.size();
+        vector<int>premx(n);
+        vector<int>premn(n);
+        premx[0]=nums[0];
+        for(int i=1;i<n;i++){
+            premx[i]=max(premx[i-1],nums[i]);
+        }
+        premn[n-1]=nums[n-1];
+        for(int i=n-2;i>=0;i--){
+            premn[i]=min(premn[i+1],nums[i]);
+        }
+        for(int i=0;i<n;i++){
+            if(premx[i]-premn[i]<=k)return i;
         }
         return -1;
     }
