0cdafd91be2842c14f5aa68a66438a43ce834b42
yasharsh-ram <sca25mca008@dsu.edu.in>
2026-09-05 21:31:31 +0530

    Time: 4 ms (89.30%) | Memory: 210.8 MB (35.81%) - LeetSync

---
+++ 4285-smallest-stable-index-ii/smallest-stable-index-ii.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int firstStableIndex(vector<int>& nums, int k) {
+        int n=nums.size();
+        vector<int>premx(n);
+        vector<int>premn(n);
+        premx[0]=nums[0];
+        for(int i=1;i<n;i++){
+            premx[i]=max(premx[i-1],nums[i]);
+        }
+        premn[n-1]=nums[n-1];
+        for(int i=n-2;i>=0;i--){
+            premn[i]=min(premn[i+1],nums[i]);
+        }
+        for(int i=0;i<n;i++){
+            if(premx[i]-premn[i]<=k)return i;
+        }
+        return -1;
+    }
+};
