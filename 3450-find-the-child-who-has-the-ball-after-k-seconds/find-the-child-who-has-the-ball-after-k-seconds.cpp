a79d70f03dc889801343343d647dfaa5298ffb93
yasharsh-ram <sca25mca008@dsu.edu.in>
2026-09-08 10:12:04 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (54.01%) - LeetSync

--- 3450-find-the-child-who-has-the-ball-after-k-seconds/find-the-child-who-has-the-ball-after-k-seconds.cpp
+++ 3450-find-the-child-who-has-the-ball-after-k-seconds/find-the-child-who-has-the-ball-after-k-seconds.cpp
@@ -1,12 +1,16 @@
 class Solution {
 public:
     int numberOfChild(int n, int k) {
-        int ans=k/(n-1);
-        int count=k%(n-1);
-        if(ans%2==0){
-            return count;
+       int pos=0;
+       int dir=1;
+       for(int i=0;i<k;i++){
+        pos+=dir;
+        if(pos==n-1||pos==0){
+            dir*=-1;
         }
-        return n-1-count;
-        
+       }
+       return pos;
     }
 };
+        
+   
