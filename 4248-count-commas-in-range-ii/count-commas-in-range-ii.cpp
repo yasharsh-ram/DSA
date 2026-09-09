24593289478a34b842b12e7dad209abeb76d2859
yasharsh-ram <sca25mca008@dsu.edu.in>
2026-09-09 13:33:40 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (84.80%) - LeetSync

--- 4248-count-commas-in-range-ii/count-commas-in-range-ii.cpp
+++ 4248-count-commas-in-range-ii/count-commas-in-range-ii.cpp
@@ -1,26 +1,39 @@
+// class Solution {
+// public:
+//     long long countCommas(long long n) {
+//        long long total=0;
+//        long long a=n-999;
+//        if(a>0){
+//         total+=a;
+//         a=n-999999;
+//         if(a>0){
+//             total+=a;
+//             a=n-999999999;
+//             if(a>0){
+//                 total+=a;
+//                 a=n-999999999999;
+//                 if(a>0){
+//                     total+=a;
+//                     a=n-999999999999999;
+//                     if(a>0){
+//                         total+=a;
+//                     }
+//                 }
+//             }
+//         }
+//        }
+//        return total;
+//     }
+// };
+
 class Solution {
 public:
     long long countCommas(long long n) {
        long long total=0;
-       long long a=n-999;
-       if(a>0){
-        total+=a;
-        a=n-999999;
-        if(a>0){
-            total+=a;
-            a=n-999999999;
-            if(a>0){
-                total+=a;
-                a=n-999999999999;
-                if(a>0){
-                    total+=a;
-                    a=n-999999999999999;
-                    if(a>0){
-                        total+=a;
-                    }
-                }
-            }
-        }
+       long long x=999;
+       while(x<n){
+        total+=n-x;
+        x=x*1000+999;
        }
        return total;
     }
