1539653290e0aad12646eab2a617b93789c590c5
yasharsh-ram <sca25mca008@dsu.edu.in>
2026-09-19 22:03:24 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (68.85%) - LeetSync

---
+++ 1501-circle-and-rectangle-overlapping/circle-and-rectangle-overlapping.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    bool checkOverlap(int radius, int xCenter, int yCenter, int x1, int y1, int x2, int y2) {
+        int x = clamp(xCenter, x1, x2) - xCenter ;
+        int y = clamp(yCenter, y1, y2) - yCenter;
+
+        return x * x + y * y <=  radius *  radius;
+    }
+};
